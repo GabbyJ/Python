781d7f5691dbbdff18fe0308e0515e85624571f9
GabbyJ <GabbyJ424@gmail.com>
2019-06-05 23:48:31 -0500

    Simple calculator update
    Made the "Pick an operator" more user friendly

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,14 +1,15 @@
 num1 = float(input("Enter first number: "))
-op = input("Enter operator (+, -, /, *): ")
+print("Pick an operator:\n1. Add\n2. Subtract\n3. Divide\n4. Multiply ")
+op = input("Enter 1-4: ")
 num2 = float(input("Enter second number: "))
 
-if op == "+":
+if op == "1":
     print(num1 + num2)
-elif op == "-":
+elif op == "2":
     print(num1 - num2)
-elif op == "/":
+elif op == "3":
     print(num1 / num2)
-elif op == "*":
+elif op == "4":
     print(num1 * num2)
 else:
     print("Invalid operator")
