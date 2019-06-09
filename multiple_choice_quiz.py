c6948b21e5ab967c9bad8abdb581b61e45c1a567
GabbyJ <GabbyJ424@gmail.com>
2019-06-09 01:35:29 -0500

    New questions, percentage, more user friendly
    Created a music theme of questions. The score returns a percentage. Created title/intro and clear space to put answer

--- multiple_choice_quiz.py
+++ multiple_choice_quiz.py
@@ -1,6 +1,10 @@
-# Add more congrats/failed options (if/else)
-# Update/add questions (strings)
-# Turn scores into percentages (math, using variables)
+# Add more congrats/failed options- Grades (if/else)
+# Update/add questions (strings) √
+# Turn scores into percentages (math) √
+# Make more interactive
+    # Obvious place to put answer (update input)√
+    # Add right/wrong to each question (if/else)
+
 
 class Question:
     def __init__(self, prompt, answer):
@@ -8,23 +12,30 @@ class Question:
         self.answer = answer
 
 question_prompts = [
-    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are Strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+    "What movie used the song 'My Heart Will Go On'?\n(a) Avatar\n(b) Overboard\n(c) Titanic\n\n",
+    "\nWho sings 'Livin on a Prayer'?\n(a) Bon Jovi\n(b) AC/DC\n(c) Bruce Springsteen\n\n",
+    "\nWhat band did a remake of 'Africa' by Toto?\n(a) Maroon 5\n(b) Weezer\n(c) Linkin Park\n\n",
+    "\nWho wrote and performed the hit song 'Uptown Funk'?\n(a) Nick Jonas\n(b) Bruno Mars\n(c) Justin Timberlake\n\n",
+    "\nWhat is Lady Gaga's fan base called?\n(a) Little Monsters\n(b) Beyhive\n(c) Swifties\n\n"
 ]
 
 questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
+    Question(question_prompts[0], "c"),
+    Question(question_prompts[1], "a"),
     Question(question_prompts[2], "b"),
-
+    Question(question_prompts[3], "b"),
+    Question(question_prompts[4], "a")
 ]
 
 def run_test(questions):
     score = 0
     for question in questions:
-        answer = input(question.prompt)
+        print(question.prompt)
+        answer = input("Your answer: ")
         if answer == question.answer:
             score += 1
-    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+    print("\nYou got " + str(score) + "/" + str(len(questions)) + " correct")
+    print("Your percentage is " + str((score*100)/len(questions)) + "%")
+
+print("\nMUSIC TRIVIA GAME\nTest your musical knowledge.\n")
 run_test(questions)
