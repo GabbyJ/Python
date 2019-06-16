1b08fac193ff1339f211a6da79500f007779ab10
GabbyJ <GabbyJ424@gmail.com>
2019-06-16 15:17:40 -0500

    Complete Hangman game
    Finished hangman game, waiting on feedback from from family and peers, but pretty sure it's good.

--- hangman.py
+++ hangman.py
@@ -1,25 +1,128 @@
 import random
 
+hangman_parts = [
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|"
+"\n|"
+"\n|"
+"\n|"
+"\n______",
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|    |"
+"\n|    |"
+"\n|"
+"\n|"
+"\n______",
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|   /|"
+"\n|    |"
+"\n|"
+"\n|"
+"\n______",
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|   /|\\"
+"\n|    |"
+"\n|"
+"\n|"
+"\n______",
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|   /|\\"
+"\n|    |"
+"\n|   /"
+"\n|"
+"\n______",
+
+"––––––"
+"\n|    |"
+"\n|    O"
+"\n|   /|\\"
+"\n|    |"
+"\n|   / \\"
+"\n|"
+"\n______",
+
+" ______"
+"\n| X  X |"
+"\n| ____ |"
+"\n|    U |"
+"\n|______|",
+
+" ______"
+"\n| O  O |"
+"\n|      |"
+"\n| (__) |"
+"\n|______|",
+
+"––––––"
+"\n|    |"
+"\n|"
+"\n|"
+"\n|        O/"
+"\n|       /|"
+"\n|        |"
+"\n______  / \\"
+
+
+
+]
+
 
 def hangman():
-    secret_word = random.choice(["microsoft", "apple", "linux", "computer", "ubuntu", "technology", "keyboard"])
+    secret_word = random.choice(["microsoft", "apple", "computer", "technology", "keyboard", "python", "javascript",
+                                 "coding", "hamburger", "community", "representative", "milkshake", "dancing", "house"
+                                 "twister", "yellow", "orange", "basketball", "football", "creativity", "mother",
+                                 "family", "human", "president", "hilarious"])
     special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "+", "=", "_", "[", "]", "{", "}", "|",
                           "/", "<", ">", "?", ",", ".", "`"]
+
     guess_count = 0
     guess_limit = 6
     guesses_left = 5
     guessed_letters = []
     wrong_letters = []
 
-    print("Welcome to Hangman!\nRules: You kill him, you lose. Good luck!\n")
-    print(secret_word)
-    print("This word has " + str(len(secret_word)) + " letters in it.")
+    print("\nWELCOME TO HANGMAN!")
+    good_name = True
+    while good_name:
+        name = str(input("\nName your Hangman: ")).title()
+        if name.isspace() or name.isdigit() or name == "" or name in special_characters or len(name) <= 1:
+            print("Enter a valid name.")
+        else:
+            good_name = False
+    print(hangman_parts[8])
+    print("This is " + name + ". " + name + " doesn't want to hang. Don't kill " + name + ".")
+    print("\nRules:\nFigure out the secret word before " + name + " hangs. You kill them, you lose."
+          "\nIf you feel you know the word, just type it in at anytime! Good luck!")
+    input("\nPress Enter to start playing... ")
+    # print(secret_word)
+    print("\nThe secret word has " + str(len(secret_word)) + " letters in it.")
 
     while guess_count < guess_limit:
         letters_in_word = ""
         for letter in secret_word:
             if letter in guessed_letters:
                 letters_in_word = letters_in_word + letter
+                if letters_in_word == secret_word:
+                    print(hangman_parts[7])
+                    print("You saved " + name + "!")
+                    print("\nCongrats! You correctly guessed the word " + "'" + secret_word + "'.")
+                    play()
+                    break
             else:
                 letters_in_word = letters_in_word + "_"
 
@@ -32,39 +135,47 @@ def hangman():
             print("\nOnly guess multiple letters if you know the word... right now you don't.")
         else:
             if guess in guessed_letters or guess in wrong_letters:
-                print("Already guessed " + "'" + guess + "'")
+                print("Already guessed " + "'" + guess + "'.")
             elif guess == secret_word:
-                print("Congrats! You correctly guessed the word " + "'" + secret_word + "'")
+                print(hangman_parts[7])
+                print("You saved " + name + "!")
+                print("\nCongrats! You correctly guessed the word " + "'" + secret_word + "'.")
                 break
             elif guess in secret_word:
                 print("'" + guess + "'" + " is in the word!")
                 guessed_letters.append(guess)
             else:
-                print("'" + guess + "'" + " is NOT in the word.\n You have " + str(guesses_left) + " wrong guesses left.")
+                print(hangman_parts[guess_count])
+                print("'" + guess + "'" + " is NOT in the word.\nYou have " + str(guesses_left) + " wrong guesses left.")
                 guess_count += 1
                 guesses_left -= 1
                 wrong_letters.append(guess)
             print()
 
     while guess_limit == 6 and guess != secret_word:
-        print("Last chance. Can you guess the word?")
+        print(name + "'s about to die! Can you save them?")
+        print("\nLast chance. Can you guess the word?")
         print(letters_in_word)
-        final_guess = input("Enter the word: ")
+        final_guess = str(input("Enter the word: ")).lower()
         if final_guess == secret_word:
-            print("Congrats! You correctly guessed the word " + "'" + secret_word + "'")
+            print(hangman_parts[7])
+            print("It's a miracle! You saved " + name + "!")
+            print("\nCongrats! You correctly guessed the word " + "'" + secret_word + "'.")
             break
         elif len(final_guess) <= 2 or final_guess == "" or final_guess.isspace() or final_guess.isdigit() or final_guess in special_characters:
             print("\nEnter a valid word guess.")
 
         else:
-            print("Sorry. The word is " + "'" + secret_word + "'" + ". Better luck next time.")
+            print(hangman_parts[6])
+            print("Oh no, you killed " + name + "!")
+            print("\nSorry. The word is " + "'" + secret_word + "'" + ". Better luck next time.")
             break
 
 
 def play():
     play_again = True
     while play_again:
-        keep_playing = input("Play again? (y/n): ")
+        keep_playing = input("\nPlay again? (y/n): ")
         affirmative = ['yes', 'yeah', 'yup', 'y', 'yea', 'si', 'sí', 'oui', 'ja', 'ha', 'hai', 'da']
         negative = ['n', 'no', 'naw', 'nah', 'nope', 'non', 'nein', 'nahi', 'net', 'lie']
         if keep_playing.lower() in affirmative:
@@ -73,7 +184,7 @@ def play():
             play_again = False
             print("Thanks for playing!")
         else:
-            print("Yes or no.")
+            print("\nYes or no.")
             play()
             break
 
